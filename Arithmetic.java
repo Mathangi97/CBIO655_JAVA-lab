c134650de056a88a4266ef152c4aee7dc3a67fee
Mathangi97 <84890576+Mathangi97@users.noreply.github.com>
2021-05-31 12:38:55 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,13 @@
+class Arithmetic
+{
+ public static void main(String args[])
+ {
+  int a=20;
+  int b=10;
+  System.out.println("The numbers are: "+a+" and "+b);
+  System.out.println("Addition: "+(a+b));
+  System.out.println("Subtraction: "+(a-b));
+  System.out.println("Multiplication: "+(a*b));
+  System.out.println("Division: "+(a/b));
+ }
+}
---
+++ Dna.java
@@ -0,0 +1,8 @@
+class Dna
+{
+ public static void main(String args[])
+ {
+  String dna="ATACGATACAA";
+  System.out.println("The DNA sequence is: "+dna);
+ }
+}
