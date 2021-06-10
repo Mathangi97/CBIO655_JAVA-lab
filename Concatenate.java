1368f51059dcd6db4d12c210415ce616b23f3728
Mathangi97 <84890576+Mathangi97@users.noreply.github.com>
2021-06-10 20:08:28 +0530

    Delete Concatenate.java

--- Concatenate.java
+++
@@ -1,9 +0,0 @@
-class Concatenate
-{
- public static void main(String args[])
- {
-  String dna1="ATATATATAT";
-  String dna2="GCGCGCGCGC";
-  System.out.println("Concatenated sequence: "+dna1+dna2);
- }
-}
